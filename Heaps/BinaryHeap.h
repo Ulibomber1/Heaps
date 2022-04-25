6af52074507f7db1f18000d5aa8cb54457fedf60
Arius <ulibomber1@gmail.com>
2022-04-25 12:52:06 -0700

    Add project files.

---
+++ Heaps/BinaryHeap.h
@@ -0,0 +1,114 @@
+#ifndef BINARY_HEAP_H
+#define BINARY_HEAP_H
+
+#include <cassert>
+#include "Vector.h"
+
+template <typename T>
+class BinaryHeap
+{
+public:
+	//constructor: empty heap with capacity 100+1
+	BinaryHeap(int capacity = 100)
+		: theheap(capacity + 1), theSize(0)
+	{}
+
+	//constructor: initialize with any vector:
+	//will convert Vector to Heap;
+	BinaryHeap(const Vector<T>& items)
+		: theheap(items.size() + 10), theSize(items.size())
+	{
+		// copy items to heap; leave field at index 0 unoccupied
+		for (int i = 0; i < items.size()l ++i)
+			theheap[i + 1] = items[i];
+		buildHeap();
+	}
+
+	bool empty() const
+	{
+		return theSize == 0;
+	}
+
+	// Find the smallest item in the heap
+	const T& findmin() const
+	{
+		assert(!theheap.empty());
+		return theheap[1];
+	}
+
+	// Insert item x, allowing duplicates;
+	void insert(const T& x)
+	{
+		if (theSize == theheap.size() - 1)
+			theheap.resize(theheap.size() * 2);
+		int hole = theSize + 1; // index of new field
+		theSize++;
+
+		T copy = x;
+		// unused position 0 to hold new value until proper place is found
+		theheap[0] = copy;
+
+		// moving up parents as long as parent has larger value than x; also called "percolate up"
+		for (; x < theheap[hole / 2]; hole /= 2)
+			theheap[hole] = theheap[hole / 2];
+		theheap[hole] = theheap[0];// Drop new value into place
+	}
+
+	//remove the minimum item
+	void deleteMin()
+	{
+		assert(!theheap.empty());
+		theheap[1] = theheap[theSize];
+		theSize--;
+		percolateDown(1);
+	}
+	// Removes the minimum item AND places it in output parameter minItem;
+	void deleteMin(T& minItem)
+	{
+		assert(!theheap.empty());
+		minItem = theheap[1];
+		theheap[1] = theheap[theSize];
+		theSize--;
+		percolateDown(1);
+	}
+
+	makeEmpty()
+	{
+		theSize = 0;
+	}
+
+
+
+
+private:
+	int theSize;
+	Vector<T> theheap;
+
+	void buildHeap()
+	{
+		for (int i = theSize / 2; i > 0; --i)
+			percolateDown(i);
+	}
+
+	void percolateDown(int hole)
+	{
+		int child;
+		T tmp = theheap[hole];
+
+		for (; hole * 2 <= theSize; hole = child)
+		{
+			child = hole * 2;
+			if (child != theSize && theheap[child + 1] < theheap[child])
+				++child;
+
+			if (theheap[child] < tmp)
+				theheap[hole] = theheap[child];
+
+			else
+				break;
+		}
+		theheap[hole] = tmp;
+	}
+
+};
+#endif
